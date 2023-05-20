c546b493049dd3b9e5f9048a386743ad34110d29
Abhijeet-kadance <Abhijeet.kadance.96@gmail.com>
2023-05-21 04:20:42 +0530

    Added Hero Section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,10 @@ import { ATLogo ,logo, menu, close } from "../assets";
 const Navbar = () => {
 
   const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
     <nav
-    className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-2 `} >
+    className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 `} >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
       <Link
           to='/'
@@ -19,7 +20,7 @@ const Navbar = () => {
           }}
         >
           <img src={ATLogo} alt='logo' className='w-9 h-9 object-contain' />
-          <p className="text-white text-[18px] font-bold cursor-pointer">Abhijeet <span className="sm:block hidden">| Portfolio </span></p>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">Abhijeet &nbsp; <span className="sm:block hidden">| &nbsp;Portfolio </span></p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link)=>(
@@ -30,6 +31,25 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+            <img src={toggle ? close : menu } alt="menu" className='w-[28px] h-[28px] object-content cursor-pointer' 
+            onClick={()=> setToggle(!toggle)}/>
+            <div className={`${!toggle ? 'hidden':'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
+          {navLinks.map((link)=>(
+            <li key={link.id} className={`${ active === link.title ? "text-white" : "text-secondary" } font-poppins font-medium cursor-pointer text-[16px]`}
+            onClick={() => {
+              setToggle(!toggle);
+              setActive(link.title);
+            }}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+            </div>
+        </div>
       </div>
     </nav>
   )
